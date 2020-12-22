f3991a411d21c7fa0009784fba93a5870ca5cedf
briann-simba <athanasbrian99@gmail.com>
2020-12-21 17:26:17 -0800

    Finished up on Registration and Login Functionalites

--- config.py
+++ config.py
@@ -3,4 +3,6 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+
 
